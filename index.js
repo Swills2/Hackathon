f993b3cf88fe55e6bf848df1a20954b36803fa56
Scott <swills0055@gmail.com>
2018-12-08 14:16:40 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const accountSid = 'AC6a2fa90cd681242f9d88219e53285ceb';
+const authToken = '87d127dd62e08ede658693d61ce7063b';
+const client = require('twilio')(accountSid, authToken);
+
+exports.handler = async (event) => {
+    console.log("Sending that ish")
+
+    var myarr = ['+18189847959', '+16262611797', '+18189124903'];
+    for (let i =0; i<myarr.length; i++) {
+        client.messages
+        .create({
+            body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+            from: '+12136992343',
+            to: myarr[i]
+        })
+        .then(message => console.log(message.sid))
+        .done();
+    }
+
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
+
+exports.handler()
+
